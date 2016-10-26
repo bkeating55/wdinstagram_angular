a9407543726b55d90f44d1d5df4b9b7bd62567e3
William Keating <keating49@gmail.com>
2016-10-26 17:55:17 -0400

    dummy data on page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let wdinstagramData = [
-  { photo_url: "http://www.visittheusa.com/~/media/7b11eeb486b9474ca0b0970c0040ec55/01%20capitol%20credit%20michael%20kleinberg%20cropped%201046x380.jpg?w=1046&h=380&bg=ffffff", author: "Billy", body: "#DC"},
+  { photo_url: "https://media-cdn.tripadvisor.com/media/photo-s/06/24/d9/8d/us-capitol.jpg", author: "Billy", body: "#DC"},
   { photo_url: "https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2d/f2/new-york-city.jpg", author: "Bob", body: "#NYC"},
   { photo_url: "http://assets.lapdonline.org/assets/jpg/los-angeles-by-josh-harris-large.jpg", author: "Jon", body: "#LA"},
   { photo_url: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Mouth_of_Miami_River_20100211.jpg", author: "Sally", body: "#MIAMI"},
@@ -9,9 +9,14 @@ let wdinstagramData = [
 ]
 
 angular
-  .module("wdinstagramApp", [])
+  .module("wdinstagramApp", ["ui.router"])
   .controller("wdinstagramCtrl", [ wdinstagramController])
 
 function wdinstagramController () {
   this.wdinstagram = wdinstagramData
+  this.newWdinstagram = {}
+  this.addWdinstagram = function(){
+    this.wdinstagram.push(this.newWdinstagram)
+    this.newWdinstagram = {}
+  }
 }
